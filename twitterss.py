3be428d94490c6b1ce6d3fc6b497af8683de22d9
brkr19 <brkr19@zoho.com>
2018-12-06 11:34:07 -0600

    Converted to ATOM feed and cleaned up packaging process

--- twitterss.py
+++ twitterss.py
@@ -3,10 +3,10 @@ import datetime
 import hashlib
 import json
 import mimetypes
-import rfeed
 import tweepy
 
 from distutils.util import strtobool
+from feedgen.feed import FeedGenerator
 
 
 class TweetArticle(object):
@@ -14,39 +14,58 @@ class TweetArticle(object):
         self._tweet = tweet
         self._is_retweet = hasattr(tweet, 'retweeted_status')
         self._is_quote = tweet.is_quote_status
+        self._created_at = tweet.created_at.replace(
+            tzinfo=datetime.timezone.utc)
 
-        # Quoted Retweet
+        original_tweet = None
+
         if self._is_quote and self._is_retweet:
             self._title_prefix = 'Quoted RT from @'
-            original_tweet = tweet.retweeted_status.quoted_status
 
-            self._body = (f"<p>{tweet.retweeted_status.full_text}</p>"
-                          f"<p>*** Quoted @{original_tweet.user.screen_name} "
-                          f"({original_tweet.user.name}) ***</p>"
-                          f"<p>{original_tweet.full_text}</p>"
-                          .replace('\n', '\n<br />'))
+            if hasattr(tweet.retweeted_status, 'quoted_status'):
+                original_tweet = tweet.retweeted_status.quoted_status
+
+                self._body = (f"<p>{tweet.retweeted_status.full_text}</p>"
+                              "<p>*** Quoted @"
+                              f"{original_tweet.user.screen_name} "
+                              f"({original_tweet.user.name}) ***</p>"
+                              f"<p>{original_tweet.full_text}</p>")
+
+            else:
+                self._body = (f"<p>{tweet.retweeted_status.full_text}</p>"
+                              "<p>*** original tweet is unavailable ***</p>")
 
         # Quote
         elif self._is_quote and not self._is_retweet:
             self._title_prefix = 'Quote from @'
-            original_tweet = tweet.quoted_status
 
-            self._body = (f"<p>{tweet.full_text}</p>"
-                          f"<p>*** Quoted @{original_tweet.user.screen_name} "
-                          f"({original_tweet.user.name}) ***</p>"
-                          f"<p>{original_tweet.full_text}</p>"
-                          .replace('\n', '\n<br />'))
+            if hasattr(tweet, 'quoted_status'):
+                original_tweet = tweet.quoted_status
+
+                self._body = (f"<p>{tweet.full_text}</p>"
+                              "<p>*** Quoted @"
+                              f"{original_tweet.user.screen_name} "
+                              f"({original_tweet.user.name}) ***</p>"
+                              f"<p>{original_tweet.full_text}</p>")
+
+            else:
+                self._body = (f"<p>{tweet.full_text}</p>"
+                              "<p>*** original tweet is unavailable ***</p>")
 
         # Retweet
         elif self._is_retweet and not self._is_quote:
             self._title_prefix = 'RT from @'
-            original_tweet = tweet.retweeted_status
 
-            self._body = (f"<p><i>Originally tweeted by "
-                          f"@{original_tweet.user.screen_name} "
-                          f"({original_tweet.user.name})</i></p>"
-                          f"<p>{original_tweet.full_text}</p>"
-                          .replace('\n', '\n<br />'))
+            if hasattr(tweet, 'retweeted_status'):
+                original_tweet = tweet.retweeted_status
+
+                self._body = (f"<p><i>Originally tweeted by "
+                              f"@{original_tweet.user.screen_name} "
+                              f"({original_tweet.user.name})</i></p>"
+                              f"<p>{original_tweet.full_text}</p>")
+
+            else:
+                self._body = "<p>*** original tweet is unavailable ***</p>"
 
         # Regular
         else:
@@ -54,9 +73,13 @@ class TweetArticle(object):
             self._body = tweet.full_text.replace('\n', '\n<br />')
             original_tweet = tweet
 
-        if 'media' in original_tweet.entities:
+        if original_tweet and 'media' in original_tweet.entities:
             self._media_url = (
                 original_tweet.entities['media'][0]['media_url_https'])
+
+            self._body += (
+                f"<br /><img src='{self._media_url}' />")
+
         else:
             self._media_url = None
 
@@ -71,7 +94,7 @@ class TweetArticle(object):
 
     @property
     def created_at(self):
-        return self._tweet.created_at
+        return self._created_at
 
     @property
     def author_handle(self):
@@ -87,7 +110,7 @@ class TweetArticle(object):
 
     @property
     def body(self):
-        return self._body
+        return self._body.replace('\n', '\n<br />')
 
     @property
     def media_url(self):
@@ -130,7 +153,7 @@ def twitterss_handler(event, context):
     bucket = config['s3']['bucket']
     folder = config['s3']['folder']
     filename_salt = config['s3']['filename_salt']
-    rss_base_url = "https://s3.amazonaws.com/{}/{}/".format(bucket, folder)
+    feed_base_url = f"https://s3.amazonaws.com/{bucket}/{folder}/"
 
     feeds = config['feeds']
 
@@ -142,7 +165,7 @@ def twitterss_handler(event, context):
     user = api.me()
 
     for feed in feeds:
-        print("Feed Title: {}".format(feed['title']))
+        print(f"Processing Feed: {feed['title']}")
 
         max_items = feed['preferences']['max_items']
         exclude_retweets = bool(strtobool(
@@ -164,9 +187,18 @@ def twitterss_handler(event, context):
         key = f"{user.id}{user.screen_name}{''.join(lists)}{filename_salt}"
         hash_object = hashlib.sha256(key.encode('utf-8'))
         rss_key = hash_object.hexdigest()
-        file_name = rss_key + ".xml"
+        file_name = rss_key + "-atom.xml"
 
-        rss_items = {}
+        # Create the base feed
+        feed_generator = FeedGenerator()
+        feed_generator.id(feed_base_url + file_name)
+        feed_generator.title(feed['title'])
+        feed_generator.author({'name': user.screen_name})
+        feed_generator.subtitle(f"{user.screen_name}'s TwitteRSS Feed")
+        feed_generator.link(href=feed_base_url + file_name, rel='self')
+        feed_generator.language('en')
+
+        entries = {}
 
         # Get list timeline
         for list_name in lists:
@@ -176,72 +208,61 @@ def twitterss_handler(event, context):
             for tweet in tweets.items(max_items):
 
                 # Don't store duplicates
-                if tweet.id_str in rss_items.keys():
+                if tweet.id_str in entries.keys():
                     continue
 
-                tweet_article = TweetArticle(tweet)
+                entry = TweetArticle(tweet)
 
-                if exclude_retweets and tweet_article.is_retweet:
+                if exclude_retweets and entry.is_retweet:
                     continue
 
-                if require_retweets and not tweet_article.is_retweet:
+                if require_retweets and not entry.is_retweet:
                     continue
 
-                if exclude_quotes and tweet_article.is_quote:
+                if exclude_quotes and entry.is_quote:
                     continue
 
-                if require_quotes and not tweet_article.is_quote:
+                if require_quotes and not entry.is_quote:
                     continue
 
-                if exclude_tweets_with_media and tweet_article.media_url:
+                if exclude_tweets_with_media and entry.media_url:
                     continue
 
-                if require_tweets_with_media and not tweet_article.media_url:
+                if require_tweets_with_media and not entry.media_url:
                     continue
 
-                # print(tweet_article)
+                # print(entry)
 
-                if tweet_article.media_url:
+                # Create the feed item
+                feed_entry = feed_generator.add_entry()
+                feed_entry.id(entry.url)
+                feed_entry.pubDate(entry.created_at)
+                feed_entry.title(entry.title)
+                feed_entry.description(entry.body)
+                feed_entry.author(name=entry.author_name)
+                feed_entry.link(href=entry.url)
+
+                if entry.media_url:
                     tweet_media_type = mimetypes.guess_type(
-                        tweet_article.media_url)[0]
+                        entry.media_url)[0]
 
-                    media = rfeed.Enclosure(
-                        url=tweet_article.media_url,
+                    feed_entry.enclosure(
+                        url=entry.media_url,
                         length=0,
                         type=tweet_media_type)
-                else:
-                    media = None
 
-                item = rfeed.Item(
-                    title=tweet_article.title,
-                    link=tweet_article.url,
-                    description=tweet_article.body,
-                    author=tweet_article.author_name,
-                    guid=rfeed.Guid(tweet_article.id, isPermaLink=False),
-                    pubDate=tweet_article.created_at,
-                    enclosure=media
-                )
-
-                rss_items[tweet_article.id] = item
-
-        feed = rfeed.Feed(
-            title=feed['title'],
-            link=rss_base_url,
-            description="{}'s TwitteRSS Feed".format(user.screen_name),
-            language="en-US",
-            lastBuildDate=datetime.datetime.now(),
-            items=list(rss_items.values()))
+                entries[entry.id] = feed_entry
 
         # Save to S3
         s3 = boto3.resource("s3")
         s3.Bucket(bucket).put_object(
             Key=folder + "/" + file_name,
-            Body=feed.rss(),
+            Body=feed_generator.atom_str(pretty=True),
             ACL='public-read',
             ContentType='application/xml',
             CacheControl='max-age=300',
             ContentEncoding='utf-8')
 
-        print(f"Saved {len(rss_items)} records to {rss_base_url}{file_name}")
+        print(f"Saved {len(entries)} records to {feed_base_url}{file_name}")
 
     return "DONE"
